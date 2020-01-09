1ca6bf218ca0b001cdd02b2be96a7a76d43ab301
Pavel <PaulEfimov2001@yandex.ru>
2020-01-10 00:57:42 +0300

    MVC not done

--- Coursework/urls.py
+++ Coursework/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
-from Coursework import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    re_path('^404', views.error404)
+    re_path('^404', views.error404),
 ]
--- Coursework/views.py
+++ Coursework/views.py
@@ -1,16 +1,35 @@
 import json
-from http.client import HTTPResponse
 
-from django.http import HttpResponse
 from django.http import Http404
-from django.http import HttpResponseNotFound
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from Coursework.forms import Nameform
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'login.html', {})
+
+
+def indexhttp(request):
+    return HttpResponse("123")
 
 
 def error404(request):
     return Http404("123")
 
+
+# def get_name(request):
+#     if request.method == 'POST':
+#         form = Nameform(request.POST)
+#         if form.is_valid():
+#             return HttpResponse('/thanks/')
+#     else:
+#         form = Nameform()
+#
+#     return render(request, 'name.html', {'form': form})
+
+
+# Reading data.json
+with open("Data/data.json", 'rb') as read_file_json:
+    data = json.load(read_file_json)
