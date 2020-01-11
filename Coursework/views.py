aeee0d9e31caa1c1e6b9aaf6b085dfbc53c6f80a
Pavel <PaulEfimov2001@yandex.ru>
2020-01-11 22:16:49 +0300

    Login

--- Coursework/views.py
+++ Coursework/views.py
@@ -3,14 +3,36 @@ import json
 from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import LoginForm
 
 
+@csrf_exempt
 def index(request):
-    login = LoginForm()
-    # login = LoginForm(request.POST)
-    return render(request, 'index.html', {'form': login})
+    loginform = LoginForm()
+    page = 'index.html'
+    if request.GET:
+        with open("Data/users.json", 'rb') as read_file_json:
+            users = json.load(read_file_json)
+        req = request.GET
+        # Проверка входа в систему
+        checkLogin = req.get("username")
+        checkPass = req.get("password")
+        checkFunc = "none"
+        for i in users["users"]:
+            if i["Login"] == checkLogin and i["Password"] == checkPass:
+                checkFunc = i["Function"]
+            if checkFunc == "Admin":
+                page = "admin.html"
+                break
+            elif checkFunc == "Moderator":
+                page = "moderator.html"
+                break
+            elif checkFunc == "user":
+                page = "user.html"
+                break
+    return render(request, page, {'form': loginform})
 
 
 def indexhttp(request):
