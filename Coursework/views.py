df91500acc014b5d58c384f59280da9c87fe82b4
Pavel <PaulEfimov2001@yandex.ru>
2020-01-15 17:58:47 +0300

    test global

--- Coursework/views.py
+++ Coursework/views.py
@@ -8,7 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .forms import LoginForm
 
-
+# Глобальные переменные
+Loginglobal = "none"
+Passglobal = "none"
+Funcglobal = "none"
+Nameglobal = "none"
 # Обработка главной страницы
 @csrf_exempt
 def index(request):
@@ -24,10 +28,14 @@ def index(request):
         for i in users["users"]:
             if i["Login"] == checkLogin and i["Password"] == checkPass:
                 checkFunc = i["Function"]
-                request.session.set_expiry(15)
-                request.session['in'] = True
-                request.session['login'] = i['login']
-                request.session['Function'] = i['Function']
+                Loginglobal = checkLogin
+                Passglobal = checkPass
+                Funcglobal = checkFunc
+                Nameglobal = i["Name"]
+                # request.session.set_expiry(15)
+                # request.session['in'] = True
+                # request.session['login'] = i['login']
+                # request.session['Function'] = i['Function']
                 break
         if request.session['Function'] == "Admin":
             return redirect("/admin")
@@ -40,7 +48,12 @@ def index(request):
 
 # Обработка страницы админа
 def adminrender(request):
-    return render(request, "admin.html")
+    with open("Data/data.json", 'rb') as read_file_json:
+        data = json.load(read_file_json)
+        Port = data["Port"]
+
+    dict_port = {'Port': Port}
+    return render(request, "admin.html", dict_port)
 
 
 # Обработка страницы модератора
