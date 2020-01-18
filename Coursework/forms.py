4684da08eec042f6088d8326f437d66fe9da0b4c
Pavel <PaulEfimov2001@yandex.ru>
2020-01-19 01:47:30 +0300

    adduser

--- Coursework/forms.py
+++ Coursework/forms.py
@@ -6,7 +6,6 @@ class LoginForm(forms.Form):
     password = forms.CharField(label="Пароль ", max_length=100)
 
 class AddUser (forms.Form):
-    Name = forms.CharField(max_length=50)
-    Login = forms.CharField(max_length=50)
-    Password = forms.CharField(max_length=50)
-    Function = forms.TypedChoiceField()
+    Name = forms.CharField(label="Имя",max_length=50)
+    Login = forms.CharField(label="Логин",max_length=50)
+    Password = forms.CharField(label="Пароль",max_length=50)
